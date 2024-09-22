0bfcad486d47f629b82f8095ada0672fa5c41279
joseverticurl <joseph.ambrose@verticurl.com>
2024-09-22 12:28:39 +0530

    circle-text-code

---
+++ blocks/circletext/circletext.js
@@ -0,0 +1,15 @@
+export default function decorate(block) { 
+    block.classList.add('row'); 
+   block.classList.add('circle');    
+  const cols = [...block.firstElementChild.children];
+  [...block.children].forEach((row) => {
+     row.classList.add('circle-container');
+     row.classList.add('col-4');
+     [...row.children].forEach((col) => {
+        const ptag = [...col.children];
+        ptag[0].classList.add('uppercase');
+        ptag[1].classList.add('ptagdesc');
+        
+    });
+  });
+}
