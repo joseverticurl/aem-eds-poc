5cdb7bb01919a9ce7948dd3e9af744b5ed9e6afa
joseverticurl <joseph.ambrose@verticurl.com>
2024-09-23 10:34:14 +0530

    Indentation change-2

--- blocks/circletext/circletext.js
+++ blocks/circletext/circletext.js
@@ -1,15 +1,13 @@
-export default function decorate(block) { 
-  block.classList.add('row'); 
-  block.classList.add('circle');    
-  const cols = [...block.firstElementChild.children];
+export default function decorate(block) {
+  block.classList.add('row');
+  block.classList.add('circle');
   [...block.children].forEach((row) => {
     row.classList.add('circle-container');
     row.classList.add('col-4');
     [...row.children].forEach((col) => {
     const ptag = [...col.children];
-    ptag[0].classList.add('uppercase');
-    ptag[1].classList.add('ptagdesc');
-        
-    });
+      ptag[0].classList.add('uppercase');
+      ptag[1].classList.add('ptagdesc');
+      });
   });
 }
